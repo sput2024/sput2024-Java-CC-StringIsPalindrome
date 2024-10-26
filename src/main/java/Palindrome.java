bf51c4c5a3670e1dfa630275400188038d51f975
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-10-26 20:23:58 +0000

    Completed Coding Challenge: String Is Palindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,34 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        Boolean value = true;
+        int k = str.length();
+        String reversedString = "";
+    
+        for (int i = k-1;i>=0;i--){
+         //   System.out.println(i);
+         //if (str.substring(i-d,i).equals(str.substring(i+d,i+d+1))==false){
+         //   System.out.println(str.substring(i-1,i));
+         //   d = d+1;
+         //   value = false;
+         //   return false;
+         reversedString = reversedString + str.charAt(i);
+
+         }
+        // if (str.substring(i-d,i).equals(str.substring(i+d,i+2))){
+         //   d = d+1;
+         //   value = true;
+         //   System.out.println("true");
+           
+         System.out.println(reversedString);
+         System.out.println(str);
+         if(reversedString.equals(str)==true){
+            value = true;
+           
+        }
+        else 
+        value = false;
+
+        return value;
     }
 }
